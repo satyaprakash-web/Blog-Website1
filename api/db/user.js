abdba5b92a77d10a242b464e2da6469be76ad0ff
Satya Prakash tiwari <93290682+Satyaprakash934@users.noreply.github.com>
2023-05-19 23:45:43 +0530

    Update user.js

--- api/db/user.js
+++ api/db/user.js
@@ -35,7 +35,7 @@ const userSchema = new mongoose.Schema({
         default : ""
     },
 }, 
-// to store time by default
+// to store time by defaul
 {timestamps : true} );
 
 const User = new mongoose.model("User", userSchema);
