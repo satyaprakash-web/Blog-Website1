3291a174451de003df54822e5095c66ce6fff48b
Satya Prakash tiwari <93290682+Satyaprakash934@users.noreply.github.com>
2023-05-19 23:46:01 +0530

    Update post.js

--- api/db/post.js
+++ api/db/post.js
@@ -38,7 +38,7 @@ const postSchema = new mongoose.Schema({
         type: Array
     }
 }, 
-// to store time by defaulta
+// to store time by default
 {timestamps : true} );
 
 const Post = new mongoose.model("Post", postSchema);
