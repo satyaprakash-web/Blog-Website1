5997ff46ab6bd8bebea0d8d634f0fe15134546ca
Satya Prakash tiwari <93290682+Satyaprakash934@users.noreply.github.com>
2023-06-02 07:46:32 +0530

    Update UserContext.js

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 
-const   UserContext = createContext(); //created new contexts
+const   UserContext = createContext(); //created new context
 
 export default UserContext;
